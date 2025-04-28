0aed83b6e5fbe52cb924c9c8746937d1486f13d2
suraj <saluja>
2025-04-28 19:11:43 +0530

    created full page asset component

---
+++ src/Components/Common/FullPageAssetContainer/index.js
@@ -0,0 +1,38 @@
+import { FocusContext, useFocusable } from '@noriginmedia/norigin-spatial-navigation';
+import React, { useState } from 'react';
+import './index.css';
+
+function FullPageAssetContainer({ assets, onAssetPress }) {
+    const { ref, focusKey: currentFocusKey, focusSelf } = useFocusable({ focusKey: 'Assets_Container' });
+    const [isLoading, setIsLoading] = useState(true);
+    const dummyAssetBoxCount = 15;
+
+    return (
+        <>
+            {isLoading ? (
+                <div className='asset-container'>
+                    {Array.from({ length: dummyAssetBoxCount }).map((_, idx) => (
+                        <div key={idx} className={'shimmer-wrapper dummyAsset_box'} />
+                    ))}
+                    <div className="shimmer" />
+                </div>
+            ) : (
+                <FocusContext.Provider value={currentFocusKey}>
+                    <div ref={ref} style={{ display: 'flex', flexWrap: 'wrap' }}>
+                        {assets.map((asset, idx) => (
+                            <div 
+                                key={asset.id || idx}
+                                onClick={() => onAssetPress(asset)}
+                                style={{ width: 100, height: 150, background: '#999', margin: 10 }}
+                            >
+                                {asset.title}
+                            </div>
+                        ))}
+                    </div>
+                </FocusContext.Provider>
+            )}
+        </>
+    );
+}
+
+export default FullPageAssetContainer;
