a4dd509a685b2934f416f2cfcfbd108f94be5766
suraj <saluja>
2025-06-09 18:05:22 +0530

    created live tv player

--- src/Components/TestComp/index.js
+++ src/Components/TestComp/index.js
@@ -3,11 +3,12 @@ import BottomDrawer from '../Common/BottomDrawer/index.js'
 import TabbedComponent from '../Common/TabbedComponent/index.js'
 import LiveTvChannelPage from '../LiveTv/ChannelPage/index.js'
 import ChannelBanner from '../LiveTv/ChannelPage/index.js'
+import LiveTvPlayer from '../LiveTv/Player/index.js'
 // import SeekBarWithPreview from '../VirtualList/SeekWithPreview.js'
 // import VirtualizedSeekbarWithThumbnails from '../VirtualList/HorizontalList.js'
 function TestComp() {
   return (
-   <ChannelBanner />
+   <LiveTvPlayer />
   )
 }
 
