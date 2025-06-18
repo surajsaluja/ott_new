4419b6342504e4a19d98d4e16f49132b106eff11
suraj <saluja>
2025-06-18 18:02:09 +0530

    removed unused code css

--- src/Components/VideoPlayer/Popup.js
+++ src/Components/VideoPlayer/Popup.js
@@ -45,7 +45,6 @@ const Popup = ({
       style={{
         display: "flex",
         flexDirection: "row",
-        gap: "20px",
         width: "100%",
         height: "200px",
         zIndex: 10,
