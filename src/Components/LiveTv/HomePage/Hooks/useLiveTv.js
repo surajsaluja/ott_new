ccf4ed94e4f84a8890a6c7b0cacf8d12d0deba8a
suraj <saluja>
2025-06-18 13:34:58 +0530

    fixed - need to check user logged in on ive tv home page

--- src/Components/LiveTv/HomePage/Hooks/useLiveTv.js
+++ src/Components/LiveTv/HomePage/Hooks/useLiveTv.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react"
 import { fetchLiveTvHomePageData } from "../../../../Service/LiveTVService";
 import { processLiveTvCategoriesToPlaylist } from "../../../../Utils";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
+import { useUserContext } from "../../../../Context/userContext";
+import { showModal } from "../../../../Utils";
 
 export const useLiveTv = () =>{
 
     const [liveTvHomePageData,setLiveTvHomePageData] =  useState([]);
     const [liveTvBannersData,setLiveTvBannersData] = useState([]);
     const [isTvDataLoading, setIsTvDataLoading] = useState(false);
-    const history = useHistory(); 
+    const history = useHistory();
+    
+    const { userObjectId, uid, isLoggedIn } = useUserContext();
 
     const loadInitialData = async () => {
         setIsTvDataLoading(true);
@@ -29,9 +33,23 @@ export const useLiveTv = () =>{
           loadInitialData();
         }, []);
 
-        const onChannelEnterPress = ({assetData}) =>{
-          history.push('/livetvschedule',assetData);
-        }
+        const redirectToLogin = () => {
+            history.replace('/login', { from: '/' });
+          };
+        
+           const onChannelEnterPress = ({assetData}) => {
+            if (isLoggedIn && userObjectId) {
+              history.push('/livetvschedule',assetData);
+            }
+            else {
+              showModal('Login',
+                'You are not logged in !!',
+                [
+                  { label: 'Login', action: redirectToLogin, className: 'primary' }
+                ]
+              );
+            }
+          };
 
     return{
         liveTvHomePageData,
