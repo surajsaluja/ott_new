10e9d710571d0d7695eecf271c727901fce5fc7e
suraj <saluja>
2025-07-02 19:35:16 +0530

    implemented online / oflline / visibility change listenrs in radio player

--- src/Components/Radio/Player/index.js
+++ src/Components/Radio/Player/index.js
@@ -16,9 +16,9 @@ export default function RadioPlayer({ focusKey }) {
     const [isLoading, setIsLoading] = useState(true);
     const history = useHistory();
     const location = useLocation();
-     const {audioName, audioImage, audioplayUrl}  = location.state || {};
+    const { audioName, audioImage, audioplayUrl } = location.state || {};
 
-    const { ref, focusKey: currentFocusKey} = useFocusable({ focusKey });
+    const { ref, focusKey: currentFocusKey } = useFocusable({ focusKey });
 
     const onBackPress = () => {
         history.goBack();
@@ -28,11 +28,11 @@ export default function RadioPlayer({ focusKey }) {
         onBackPress();
     });
 
-    useEffect(()=>{
-        if(!isLoading){
+    useEffect(() => {
+        if (!isLoading) {
             setFocus('RADIO_PLAY_PAUSE_BUTTON_FOCUS_KEY');
         }
-    },[isLoading])
+    }, [isLoading])
 
     const handleSetIsPlaying = async (val) => {
         let audio = audioRef.current;
@@ -81,12 +81,34 @@ export default function RadioPlayer({ focusKey }) {
             handleSetIsPlaying(true);
         };
 
+        const handlePlayerOnline = () => {
+            handleSetIsPlaying(true);
+        }
+
+        const handlePlayerOffline = () => {
+            handleSetIsPlaying(false);
+        }
+
+        const handlePlayerVisibilityChange = () => {
+            console.log(document.hidden);
+            if (document.hidden) {
+                handleSetIsPlaying(false);
+                console.log('video paused');
+            } else {
+                handleSetIsPlaying(true);
+                console.log('video played');
+            }
+        }
+
         audio.addEventListener('play', onPlay);
         audio.addEventListener('pause', onPause);
         audio.addEventListener('ended', onEnded);
         audio.addEventListener('error', onError);
         audio.addEventListener('loadstart', onLoadStart);
         audio.addEventListener('canplay', onCanPlay);
+        window.addEventListener('online', handlePlayerOnline);
+        window.addEventListener('offline', handlePlayerOffline);
+        window.addEventListener('visibilitychange', handlePlayerVisibilityChange)
 
         return () => {
             audio.pause();
@@ -96,43 +118,46 @@ export default function RadioPlayer({ focusKey }) {
             audio.removeEventListener('error', onError);
             audio.removeEventListener('loadstart', onLoadStart);
             audio.removeEventListener('canplay', onCanPlay);
+            window.removeEventListener('online', handlePlayerOnline);
+            window.removeEventListener('offline', handlePlayerOffline);
+            window.removeEventListener('visibilitychange', handlePlayerVisibilityChange)
         };
     }, []);
 
     return (
         <>
-            {isLoading ? (<Spinner />) :   
-            (<FocusContext.Provider value={currentFocusKey}>
-                <div className="player" style={{ backgroundColor: 'black', height: '100vh' }} ref={ref}>
-                    <div className="radio-top-container">
-                        <FocusableButton
-                            className={'radio-back-button'}
-                            focusClass={'radio-play-pause-focused'}
-                            focuskey={'RADIO_BACK_BUTTON_FOCUS_KEY'}
-                            icon={<MdArrowBack />}
-                            onEnterPress={onBackPress}
-                        />
-                        <div className='radio-title-wrapper'>
-                            {audioName}
+            {isLoading ? (<Spinner />) :
+                (<FocusContext.Provider value={currentFocusKey}>
+                    <div className="player" style={{ backgroundColor: 'black', height: '100vh' }} ref={ref}>
+                        <div className="radio-top-container">
+                            <FocusableButton
+                                className={'radio-back-button'}
+                                focusClass={'radio-play-pause-focused'}
+                                focuskey={'RADIO_BACK_BUTTON_FOCUS_KEY'}
+                                icon={<MdArrowBack />}
+                                onEnterPress={onBackPress}
+                            />
+                            <div className='radio-title-wrapper'>
+                                {audioName}
+                            </div>
+                        </div>
+                        <div className='image-container-radio'>
+                            <img
+                                src={audioImage}
+                                className='radio-image'
+                            />
+                        </div>
+                        <div className='radio-bottom-container'>
+                            <FocusableButton
+                                className={'radio-play-pause'}
+                                focusClass={'radio-play-pause-focused'}
+                                focuskey={'RADIO_PLAY_PAUSE_BUTTON_FOCUS_KEY'}
+                                icon={isPlaying ? <MdOutlinePause /> : <MdPlayArrow />}
+                                onEnterPress={togglePlayPause}
+                            />
                         </div>
                     </div>
-                    <div className='image-container-radio'>
-                        <img
-                            src={audioImage}
-                            className='radio-image'
-                        />
-                    </div>
-                    <div className='radio-bottom-container'>
-                        <FocusableButton
-                            className={'radio-play-pause'}
-                            focusClass={'radio-play-pause-focused'}
-                            focuskey={'RADIO_PLAY_PAUSE_BUTTON_FOCUS_KEY'}
-                            icon={isPlaying ? <MdOutlinePause /> : <MdPlayArrow />}
-                            onEnterPress={togglePlayPause}
-                        />
-                    </div>
-                </div>
-            </FocusContext.Provider>)}
+                </FocusContext.Provider>)}
             <audio ref={audioRef} style={{ display: 'none' }} />
         </>
     );
