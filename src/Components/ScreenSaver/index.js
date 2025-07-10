1941ba8c6609ecf1ff97da8f48f09fc7a8962506
suraj <saluja>
2025-07-10 16:57:27 +0530

    screen saver buttons made not to rerender on screen saver change

--- src/Components/ScreenSaver/index.js
+++ src/Components/ScreenSaver/index.js
@@ -2,6 +2,26 @@ import { useFocusable, FocusContext } from '@noriginmedia/norigin-spatial-naviga
 import './index.css';
 import useScreenSaver from './Hooks/useScreenSaver';
 import FocusableButton from '../Common/FocusableButton';
+import React, { memo } from 'react';
+
+const ScreenSaverContent = memo(({ resource }) => {
+  if (!resource) return null;
+
+  return (
+    <div className="screen-saver-screen-item active" key={resource.title}>
+      <img src={resource.fullPageBanner} alt={resource.title} />
+      <div className="screen-saver-info">
+        <h1 className="montserrat-extrabold">{resource.title}</h1>
+        <ul className="screen-saver-genres">
+          {resource.genre?.split(',').map((gr, i) => (
+            <li key={i} className={i > 0 ? 'with-dot' : ''}>{gr}</li>
+          ))}
+        </ul>
+        <h2>{resource.caption}</h2>
+      </div>
+    </div>
+  );
+});
 
 const Screensaver = () => {
   const {
@@ -18,36 +38,25 @@ const Screensaver = () => {
   return (
     <FocusContext.Provider value={currentFocusKey}>
       <div className="screensaver" ref={ref}>
-          {current && <div className="screen-saver-screen-item active" key={current.title}>
-            <img src={current.fullPageBanner} alt={current.title} />
-            <div className="screen-saver-info">
-              <h1 className="montserrat-extrabold">{current.title}</h1>
-              <ul className="screen-saver-genres">
-                {current.genre?.split(',').map((gr, i) => (
-                  <li key={i} className={i > 0 ? 'with-dot' : ''}>{gr}</li>
-                ))}
-              </ul>
-              <h2>{current.caption}</h2>
+        <ScreenSaverContent resource={current} />
 
-              {/* 👇 Make button container a focusable parent */}
-              <div className='screen-saver-buttons-container'>
-                <FocusableButton
-                  className='screen-saver-button'
-                  focusClass='screen-saver-button-focused'
-                  text='Watch Now'
-                  focusKey='SCREEN_SAVER_WATCH_BUTTON'
-                  onEnterPress={onWatchClipSS}
-                />
-                <FocusableButton
-                  className='screen-saver-button'
-                  focusClass='screen-saver-button-focused'
-                  text='More Info'
-                  focusKey='SCREEN_SAVER_DETAIL_BUTTON'
-                  onEnterPress={onMoreInfoItemClickSS}
-                />
-              </div>
-            </div>
-          </div>}
+        {/* Buttons should not rerender on screen content change */}
+        <div className='screen-saver-buttons-container'>
+          <FocusableButton
+            className='screen-saver-button'
+            focusClass='screen-saver-button-focused'
+            text='Watch Now'
+            focusKey='SCREEN_SAVER_WATCH_BUTTON'
+            onEnterPress={onWatchClipSS}
+          />
+          <FocusableButton
+            className='screen-saver-button'
+            focusClass='screen-saver-button-focused'
+            text='More Info'
+            focusKey='SCREEN_SAVER_DETAIL_BUTTON'
+            onEnterPress={onMoreInfoItemClickSS}
+          />
+        </div>
       </div>
     </FocusContext.Provider>
   );
